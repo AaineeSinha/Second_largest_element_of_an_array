f2329e47071899aebd6009c58ca9911d6e7cf88c
AaineeSinha <aaineesinhajhr16@gmail.com>
2025-07-31 23:35:33 +0530

    Add files via upload

---
+++ second largest element of an array in python.py
@@ -0,0 +1,11 @@
+# Second_largest_element_of_an_array
+class Solution:
+    def getSecondLargest(self, arr):
+        first = second = -1
+        for num in arr:
+            if num > first:
+                second = first
+                first = num
+            elif num > second and num != first:
+                second = num
+        return second
